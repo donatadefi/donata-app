9e11b4bf96c6e4427b523022d61415279f290d4a
donatadefi <donatadefi@protonmail.com>
2021-06-22 10:02:02 +0800

    add sidebar logo

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import logo from '../assets/donata-logo.png';
+import './Sidebar.scss';
 
 function Sidebar() {
   return (
     <div className="Sidebar">
-      <p>Sidebar</p>
+      <div className="sidebar-logo">
+        <img src={logo} alt="" />
+        <h3>DONATA</h3>
+      </div>
     </div>
   );
 }
