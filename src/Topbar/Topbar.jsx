307f565c8ab73691fb1c778a4e7f793cee6d2cf3
donatadefi <donatadefi@protonmail.com>
2021-06-23 09:23:14 +0800

    add topbar layout

--- src/Topbar/Topbar.jsx
+++ src/Topbar/Topbar.jsx
@@ -5,7 +5,11 @@ import './Topbar.scss';
 function Topbar() {
   return (
     <div className="Topbar">
-      <p>Topbar</p>
+      <img
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn3.iconfinder.com%2Fdata%2Ficons%2Favatars-round-flat%2F33%2Fman5-512.png&f=1&nofb=1"
+        alt=""
+      />
+      <button>CONNECT</button>
     </div>
   );
 }
