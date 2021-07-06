84bfcaef0ad7e2bc03764de4ca131238801c1edb
donatadefi <donatadefi@protonmail.com>
2021-07-06 21:16:30 +0800

    add socials input

--- src/Setting/Setting.jsx
+++ src/Setting/Setting.jsx
@@ -8,12 +8,27 @@ var sigUtil = require('eth-sig-util');
 
 function Setting({ account }) {
   const [description, setDescription] = useState('');
+  const [photoUrl, setPhotoUrl] = useState('');
+  const [socials, setSocials] = useState({});
+  const [tokens, setTokens] = useState({});
+
   const putDescription = (e) => {
     setDescription(e.target.value);
   };
 
+  const putSocials = (e, social) => {
+    setSocials((state) => {
+      return { ...state, [social]: e.target.value };
+    });
+  };
+
   const signData = (type) => {
-    if (!description) {
+    if (
+      (!description && type === 'description') ||
+      (!photoUrl && type === 'photoUrl') ||
+      (Object.keys(socials).length === 0 && type === 'socials') ||
+      (Object.keys(tokens).length === 0 && type === 'tokens')
+    ) {
       return;
     }
     const msgParams = JSON.stringify({
@@ -41,6 +56,15 @@ function Setting({ account }) {
     const content = () => {
       if (type == 'description') {
         return description;
+      }
+      if (type == 'photoUrl') {
+        return photoUrl;
+      }
+      if (type == 'socials') {
+        return socials;
+      }
+      if (type == 'tokens') {
+        return tokens;
       }
     };
     window.ethereum.sendAsync(
@@ -86,7 +110,6 @@ function Setting({ account }) {
             body: JSON.stringify(reqBody),
           })
             .then((res) => {
-              console.log(res);
               return res.json();
             })
             .then((status) => {
@@ -141,7 +164,10 @@ function Setting({ account }) {
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
             </svg>
-            <Input placeholder="URL" />
+            <Input
+              placeholder="URL"
+              onChange={(e) => putSocials(e, 'twitter')}
+            />
           </div>
           <div className="social-input">
             <svg
@@ -160,7 +186,10 @@ function Setting({ account }) {
               <rect x="3" y="5" width="18" height="14" rx="4" />
               <path d="M10 9l5 3l-5 3z" />
             </svg>
-            <Input placeholder="URL" />
+            <Input
+              placeholder="URL"
+              onChange={(e) => putSocials(e, 'youtube')}
+            />
           </div>
           <div className="social-input">
             <svg
@@ -180,7 +209,10 @@ function Setting({ account }) {
               <line x1="16" y1="8" x2="16" y2="12" />
               <line x1="12" y1="8" x2="12" y2="12" />
             </svg>
-            <Input placeholder="URL" />
+            <Input
+              placeholder="URL"
+              onChange={(e) => putSocials(e, 'twitch')}
+            />
           </div>
           <div className="social-input">
             <svg
@@ -200,7 +232,10 @@ function Setting({ account }) {
               <circle cx="12" cy="12" r="3" />
               <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
             </svg>
-            <Input placeholder="URL" />
+            <Input
+              placeholder="URL"
+              onChange={(e) => putSocials(e, 'instagram')}
+            />
           </div>
           <div className="social-input">
             <svg
@@ -218,7 +253,10 @@ function Setting({ account }) {
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M9 12a4 4 0 1 0 4 4v-12a5 5 0 0 0 5 5" />
             </svg>
-            <Input placeholder="URL" />
+            <Input
+              placeholder="URL"
+              onChange={(e) => putSocials(e, 'tiktok')}
+            />
           </div>
           <div className="social-input">
             <svg
@@ -236,9 +274,14 @@ function Setting({ account }) {
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
             </svg>
-            <Input placeholder="URL" />
+            <Input
+              placeholder="URL"
+              onChange={(e) => putSocials(e, 'facebook')}
+            />
           </div>
-          <button className="set-action">Confirm</button>
+          <button className="set-action" onClick={() => signData('socials')}>
+            Confirm
+          </button>
         </div>
 
         <div>
